a00d2e323cdfb958ad0a0656deb3ad1f50bac492
Hsu <edittye379@gmail.com>
2017-01-19 00:49:52 +0800

    vue-router Navigation Links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,22 @@
     <div class="header clearfix">
         <nav>
             <ul class="nav nav-pills pull-right">
-                <li role="presentation" class="active"><a href="/">Home</a></li>
-                <li role="presentation"><a href="/#/login">Login</a></li>
-                <li role="presentation"><a href="/#/register">Register</a></li>
+                <router-link tag="li" to="/">
+                    <a>Home</a>
+                </router-link>
+                <router-link tag="li" to="/login" v-if="! isAuth">
+                    <a>Login</a>
+                </router-link>
+                <router-link tag="li" to="/register" v-if="! isAuth">
+                    <a>Register</a>
+                </router-link>
+
+                <router-link tag="li" to="/login" v-if="isAuth">
+                    <a>Feed</a>
+                </router-link>
+                <router-link tag="li" to="/register" v-if="isAuth">
+                    <a>Lougut</a>
+                </router-link>
             </ul>
         </nav>
         <h3 class="text-muted">Project name</h3>
@@ -13,6 +26,14 @@
 
 <script>
 export default {
+    data () {
+        return {
+            isAuth: null
+        }
+    },
+    created () {
+        this.isAuth = this.$auth.isAuthenticated()
+    }
 }
 </script>
 
