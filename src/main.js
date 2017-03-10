7c410ac4515db33fec1bbf5d0aebd577792b819c
Hsu <edittye379@gmail.com>
2017-03-10 22:17:09 +0800

    Handling Errors Globally Using Vue.http.interceptors

--- src/main.js
+++ src/main.js
@@ -10,6 +10,16 @@ Vue.use(Auth)
 Vue.http.options.root = 'http://back.dev'
 Vue.http.headers.common['Authorization'] = 'Bearer ' + Vue.auth.getToken()
 
+Vue.http.interceptors.push((request, next) => {
+    next(response => {
+        if (response.status == 404) {
+            swal(response.status.toString(), response.body.error, "error")
+        } else if (response.status == 500) {
+            swal(response.status.toString(), "We are experiencing a problem in our servers!", "error")
+        }
+    })
+})
+
 Router.beforeEach(
     (to, from, next) => {
         if (to.matched.some(record => record.meta.forVisitors)) { // if forVisitors(登入者可進入)
